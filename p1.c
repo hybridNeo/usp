2eb593ca44f7115273936b6f79161b1325ca833f
rahul.mahadev7@gmail.com <rahul.mahadev7@gmail.com>
2015-01-19 08:47:18 +0530

    Week 2

--- p1.c
+++ p1.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-int main(){
-	char *buf[100];
-	int a = read(0,buf,100);
-	write(1,buf,a);
+#include <string.h>
+int main(void){
+	char str[100];
+	printf("Enter some text to write \n");
+	gets(str);
+	int fd,sa;
+	fd = open("out.txt",O_RDWR|O_CREAT|O_APPEND,0644);
+	if(fd==-1)
+		printf("Error writing to file\n");
+	else{
+		
+	 	sa = write(fd,str,strlen(str));
+	}
+	
+	int eof = lseek(fd,0,SEEK_CUR);
+	char write_buf[eof];
+	lseek(fd,0,SEEK_SET);
+	read(fd,write_buf,eof);
+	printf("*****DATA**READ*****\n");
+	printf("%s",write_buf);
+	close(fd);	
+	
 }
